54b84d51ef17d0a5c1e54466d1970c59a03436f8
Hylan-J <1879159965@QQ.COM>
2025-04-09 16:30:03 +0800

    update, can run

--- include/ble/whitening.h
+++ include/ble/whitening.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-const uint8_t whitening_tables[40][42];
+extern const uint8_t whitening_tables[40][42];
 void dewhitening_bytes(uint8_t *byte_in, int num_byte, const uint8_t *scramble_table_byte, uint8_t *byte_out);
 
 #endif // WHITENING_H
