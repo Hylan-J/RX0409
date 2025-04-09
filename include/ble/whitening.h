db7fc4097b056f89305c1005ab40b3c4f2773be7
Hylan-J <1879159965@QQ.COM>
2025-04-09 15:05:37 +0800

    update

---
+++ include/ble/whitening.h
@@ -0,0 +1,9 @@
+#ifndef WHITENING_H
+#define WHITENING_H
+
+#include <stdint.h>
+
+const uint8_t whitening_tables[40][42];
+void dewhitening_bytes(uint8_t *byte_in, int num_byte, const uint8_t *scramble_table_byte, uint8_t *byte_out);
+
+#endif // WHITENING_H
